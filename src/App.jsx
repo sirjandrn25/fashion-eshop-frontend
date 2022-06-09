3feec30decc7555be203a253f31c7d85a8dc5533
sirjan tamang <sirjantmg99@gmail.com>
2022-06-09 13:16:44 +0545

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/nav-header/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='h-full relative w-screen flex flex-col justify-center items-center bg-gray-200'>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/nav-header/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from '../UI/Button'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { cartActions } from '../../redux/reducers/carts'
+
+const Navbar = () => {
+  // const { toggleOpen } = cartActions
+  // const dispatch = useDispatch()
+  // console.log(toggleOpen)
+
+  return (
+    <nav className='w-full z-40 bg-black p-5 flex flex-row justify-around items-center fixed top-0 left-0 right-0 shadow  '>
+      <div>
+        <span className='text-2xl font-bold text-white'>Daraj Shop</span>
+      </div>
+      <div>
+        <Button
+          // onClick={(e) => dispatch(toggleOpen())}
+          className='bg-black/40 border-2 flex items-center border-cyan-400 hover:bg-blue-400'>
+          <span className='text-xl font-bold mr-2'>Cart</span>
+          <span className='text-white  font-medium bg-blue-500 px-2 rounded-full'>4</span>
+        </Button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
