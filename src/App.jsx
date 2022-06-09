bdf03fde1996cfdf5fb41087946be54d38031fa7
sirjan tamang <sirjantmg99@gmail.com>
2022-06-09 13:21:36 +0545

    product setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/nav-header/Navbar'
+import ProductContainer from './components/product/ProductContainer'
 
 const App = () => {
   return (
     <div className='h-full relative w-screen flex flex-col justify-center items-center bg-gray-200'>
       <Navbar />
+      <ProductContainer />
     </div>
   )
 }
---
+++ src/components/product/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from '../UI/Card'
+import Button from '../UI/Button'
+const ProductCard = (props) => {
+  return (
+    <Card className='bg-white flex flex-col hover:shadow-lg'>
+      <div className='w-full h-[220px] flex justify-center items-center object-cover  border-b-2'>
+        <img
+          src={props.product.image}
+          className='w-full h-full scale-75 transition-all duration-100 hover:scale-95'
+          alt=''
+        />
+      </div>
+      <span className='my-2 text-lg font-semibold text-gray-600'>{props.product.title}</span>
+      <div className='mt-1 flex flex-row items-center '>
+        <Button className='border-2 border-sky-400 hover:bg-sky-500 hover:text-white text-gray-700 mr-3 text-sm'>
+          Add to Cart
+        </Button>
+        <span className='text-xl font-sans text-purple-700 font-bold'>${props.product.price}</span>
+      </div>
+    </Card>
+  )
+}
+
+export default ProductCard
